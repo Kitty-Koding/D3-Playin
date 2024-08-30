4eac7b8483b6ef740f91a8da7cb636c082f319d4
Library Kitten <126348030+Kitty-Koding@users.noreply.github.com>
2024-08-30 18:55:07 -0400

    g

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
-d3.select('div')
-    .selectAll('p')
-    .data([1,2,3])
+const DummyData = [
+    {id:'d1',value:10,region:'USA'},
+    {id:'d2',value:1,region:'China'},
+    {id:'d3',value:5,region:'India'},
+    {id:'d4',value:12,region:'Spain'},
+  ];
+  
+const container = d3.select('svg')
+    .classed('container', true)
+    .style('border','1px solid black');
+
+const bars = container
+    .selectAll('.bar')
+    .data(DummyData)
     .enter()
-    .append('p')
-    .text(dta => dta);
+    .append('div')
+    .classed('bar', true)
+    .style('width',250/DummyData.length+1+"px")
+    .style('height',data => (data.value * 15)+'px');
